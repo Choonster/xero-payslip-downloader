84abb03b311a2f160ccdb012033f2d37ecbc208e
Choonster TheMage <Choonster.2010@gmail.com>
2024-06-29 18:03:54 +1000

    Use collection expression - Remove unnecessary suppression

--- XeroPayslipDownloader/Program.cs
+++ XeroPayslipDownloader/Program.cs
@@ -7,9 +7,7 @@ builder.Services.AddHostedService<Worker>();
 builder.Services.Configure<DownloadOptions>(builder.Configuration.GetSection(DownloadOptions.Download));
 
 // Install the browser required by Playwright 
-#pragma warning disable CA1861 // Avoid constant arrays as arguments
-Microsoft.Playwright.Program.Main(new[] { "install", "firefox" });
-#pragma warning restore CA1861 // Avoid constant arrays as arguments
+Microsoft.Playwright.Program.Main(["install", "firefox"]);
 
 var host = builder.Build();
 host.Run();
